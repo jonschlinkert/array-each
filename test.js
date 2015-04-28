d7d38c1297ea0016149b13a75d51d4fa321f13d0
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-28 15:08:46 -0400

    adds index test

--- test.js
+++ test.js
@@ -27,12 +27,20 @@ describe('each', function () {
 
   it('should "break" when `false` is returned:', function () {
     var res = [];
-    each(['a', 'b', 'c'], function (ele) {
+    each(['a', 'b', 'c'], function (ele, i) {
       if (ele === 'b') {
         return false;
       }
       res.push(ele + ele);
     });
     res.should.eql(['aa']);
+  });
+
+  it('should expose the index as the second parameter:', function () {
+    var res = [];
+    each(['a', 'b', 'c'], function (ele, i) {
+      res.push(i);
+    });
+    res.should.eql([0, 1, 2]);
   });
 });
